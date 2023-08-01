b37d4748a27e4dc1ac11d334d4d55f9e3dc11b86
Danila Morozov <31007107+dmorozko@users.noreply.github.com>
2023-08-01 18:34:28 +0300

    Update ConverterBotApplication.java

--- src/main/java/com/MorozovStudio/ConverterBot/ConverterBotApplication.java
+++ src/main/java/com/MorozovStudio/ConverterBot/ConverterBotApplication.java
@@ -2,10 +2,8 @@ package com.MorozovStudio.ConverterBot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
-@PropertySource("file:D:\\JavaProject\\ConverterBot_v1.0\\application.properties")
 public class ConverterBotApplication {
 
 	public static void main(String[] args) {
